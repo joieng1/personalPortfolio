b415eb67766e0a736d8ee7d4c8ed248bec5f5482
joieng1 <joieng@s.sfusd.edu>
2021-08-05 18:19:18 -0700

    push

--- app.js
+++ app.js
@@ -2,19 +2,9 @@
 
 document.getElementById("startBtn").addEventListener("click", start);
 
-// $(document).scroll(function () {
-//     let $nav = $("#mainNavbar");
-//     $nav.toggleClass(
-//         "scrolled",
-//         $(this).scrollTop() > $nav.height()
-//     );
-// });
-
-// function scroll() {
-
-// }
-
 function start() {
+    //Responsible for displaying content on homepage after pressing button
+    
     console.log("start");
     let html = '<div class="spacer1">';
     html += '<div class="row text-center">';
