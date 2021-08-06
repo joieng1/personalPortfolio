c3fe6400e1283a24f032490dfe30530904ae97ff
joieng1 <joieng@s.sfusd.edu>
2021-08-05 17:52:57 -0700

    push

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+document.getElementById("startBtn").addEventListener("click", start);
 
 // $(document).scroll(function () {
 //     let $nav = $("#mainNavbar");
@@ -12,3 +13,18 @@
 // function scroll() {
 
 // }
+
+function start() {
+    console.log("start");
+    let html = '<div class="spacer1">';
+    html += '<div class="row text-center">';
+    html += '<div class="col">';
+    html += '<div class="col text-center mt-5">';
+    html += '<h1>Welcome</h1>';
+    html += '</div></div></div></div>';
+    setTimeout(function(){
+        document.getElementById("header").innerHTML = "";
+    }, 200);
+    
+    document.getElementById("welcomePage").innerHTML = html;
+}
